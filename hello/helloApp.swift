81f36ca1710dca7f11fe034d3902bab520b1fcb9
luciliuscode <quesnelr@mac.com>
2023-07-30 08:26:11 -0400

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by René Gagnon-Quesnel on 2023-07-30.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
